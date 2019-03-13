32ba7595cd4ff42c44581ecf8445e7ed4fb7a441
Jordan <martinj1@franklinacademy.org>
2019-03-13 14:28:56 -0400

    sd card is a temporary no go

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <RF24.h>
-#include <SD.h>
 
 /**
  * Program: Rocket Payload
@@ -12,23 +11,42 @@
  * Description: Tester for rocket payloads. Also program to develop payload code.
  * Last Edited by: Jordan Martin
  * Last Edited: 12 March 2019
- * Reason edited: Added checksum 
+ * Reason edited: Added checksum
  */
 SoftwareSerial serial_connection(3, 2); //RX=pin 3, TX=pin 2 (for arduino... connect TX to RX and vice-versa)
 TinyGPSPlus gps;//This is the GPS object that will pretty much do all the grunt work with the NMEA data
-// File dataFile;
 RF24 radio(9, 10);
 const byte address[6] = "00001";
 
-String checksum(TinyGPSLocation loc) {
-  char toReturn[100] = "";
-  double temp = loc.lat() + loc.lng();
-  int sum = 0;
-  for(char i : String(temp)){
-    sum += i;
+String checksum(float x, float y) { // general algorithm
+  char toReturn[100] = ""; // string to return
+  double temp = x + y; // combine x and y
+  int sum = 0; // init sum var
+  for(char i : String(temp)){ // go through each char in the string of temp
+    sum += i; // add ascii values to sum
   }
-  sum %= 0xff;
-  sprintf(toReturn, "*%X", sum);
+  sum %= 0xff; // limit to 2 hexadecimal characters
+  sprintf(toReturn, "*%X", sum); // convert to hexadecimal value with * in front (checksum)
+  return toReturn; // return checksum
+}
+
+String checksum(TinyGPSLocation loc) {
+  return (checksum(loc.lat(), loc.lng()));
+}
+
+String checksum(TinyGPSTime time, float alt) {
+  String hr = String(time.hour());
+  if(time.hour() < 10) hr = "0" + hr;
+  String min = String(time.minute());
+  if(time.minute() < 10) min = "0" + min;
+  String sec = String(time.second());
+  if(time.second() < 10) sec = "0" + sec;
+  float newTime = atof((hr + min + sec).c_str());
+  return (checksum(newTime, alt));
+}
+
+String floatToString(float val, int length) {
+  String toReturn = String(val, length);
   return toReturn;
 }
 
@@ -36,11 +54,11 @@ void setup()
 {
   Serial.begin(115200);//This opens up communications to the Serial monitor in the Arduino IDE
   serial_connection.begin(9600);//This opens up communications to the GPS
+  SPI.begin();
   radio.begin(); // start the radio transmitter
   radio.openWritingPipe(address); // start radio on certain address
   radio.setPALevel(RF24_PA_MIN); // sets power amplifier level
   radio.stopListening(); // stop looking for messages (this radio isn't intended to receive messages yet)
-  // dataFile = SD.open("GPSDAT.txt", FILE_WRITE);
   Serial.println("GPS Start");//Just show to the monitor that the sketch has started
 }
 
@@ -53,14 +71,22 @@ void loop()
   if(gps.location.isUpdated())//This will pretty much be fired all the time anyway but will at least reduce it to only after a package of NMEA data comes in
   {
     // compile and send location data
-    String temp = "LX:" + String(gps.location.lat(), 6) + "LY:" + String(gps.location.lng(), 6) + checksum(gps.location); // compile into string to be used later
-    char location[temp.length()+1] = ""; // create location character array
-    for(int i = 0; i < (int)temp.length(); i++) { // go through each piece of the string data
-      location[i] = temp.charAt(i); // add current piece of string to char array
-    }
+    char location[100] = ""; // create location character array
+    sprintf(location, "LX:%sLY:%s%s", floatToString(gps.location.lat(), 6).c_str(), floatToString(gps.location.lng(), 6).c_str(), checksum(gps.location).c_str());
     radio.write(&location, sizeof(location)); // send location data to receiver through the radio
     Serial.println(location);
-    // dataFile.println(gps.getRawData());
-    delay(50);
+
+    // compile and send altitude data
+    char altitude[100] = ""; // create altitude charachter array
+    String hr = String(gps.time.hour());
+    if(gps.time.hour() < 10) hr = "0" + hr;
+    String min = String(gps.time.minute());
+    if(gps.time.minute() < 10) min = "0" + min;
+    String sec = String(gps.time.second());
+    if(gps.time.second() < 10) sec = "0" + sec;
+    String compiledTime = hr + min + sec;
+    sprintf(altitude, "PA:%sTS:%s%s", floatToString(gps.altitude.meters(), sizeof(gps.altitude.meters())/sizeof(double)).c_str(), compiledTime.c_str(), checksum(gps.time, gps.altitude.meters()).c_str());
+    radio.write(&altitude, sizeof(altitude)); // send altitude data to receiver through the radio
+    Serial.println(altitude);
   }
 }
